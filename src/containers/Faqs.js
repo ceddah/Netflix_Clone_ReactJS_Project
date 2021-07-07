0d59f36116bf0a3928915b848ace3f9bf811498b
ceddah <cedanik1@gmail.com>
2021-07-07 14:18:18 +0200

    Opt-Form Component

--- src/containers/Faqs.js
+++ src/containers/Faqs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 import faqsData from '../fixtures/faq.json';
 
 export const FaqsContainer = () => {
@@ -14,7 +14,12 @@ export const FaqsContainer = () => {
                     </Accordion.Item>
                 )}
             </Accordion.Frame>
-            
+            <OptForm>
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                <OptForm.Break />
+                <OptForm.Input placeholder="Email Address" />
+                <OptForm.Button>Try it now</OptForm.Button>
+            </OptForm>
         </Accordion>
     )
 }
