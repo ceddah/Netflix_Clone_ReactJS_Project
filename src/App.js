9139f1c7e4c9f7fe9858663c73ea18d26b5213bf
ceddah <cedanik1@gmail.com>
2021-07-08 14:07:40 +0200

    useAuthListener hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import * as ROUTES from './constants/routes';
 import {Home, Browse, Signin, Signup} from './pages';
 
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
+import { useAuthListener } from './hooks/';
+
 export default function App() {
-  const user = {};
+  const { user } = useAuthListener();
+  console.log(user);
   return (
     <Router>
       <IsUserRedirect exact user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
