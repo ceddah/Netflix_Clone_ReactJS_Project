10530521427d8cabe89b31e54b43cf8b5aaf52f9
ceddah <cedanik1@gmail.com>
2021-07-08 16:50:33 +0200

    Who's Watching Profile

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import SelectProfilesContainer from './Profies'
+import { FirebaseContext } from '../context/firebase';
 
 export default function BrowseContainer({ slides }) {
-    return (
-        <div>
-            Hello
-        </div>
-    )
+    const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
+    const { firebase } = useContext(FirebaseContext);
+    const user = firebase.auth().currentUser || {}
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000)
+    }, [profile.displayName])
+
+    return profile.displayName ? (
+        loading ? <Loading src={user.photoURL} /> : null
+    ) : <SelectProfilesContainer user={user} setProfile={setProfile} />
 }
